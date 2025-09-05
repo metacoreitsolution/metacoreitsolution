4efbe0000a1a42ff184403865985fd24e8f88395
brijesh dhorajiya <dhorajiyabrijesh607@outlook.com>
2025-09-05 23:13:51 +0530

    change whatsapp number

--- assets/js/whatsapp.js
+++ assets/js/whatsapp.js
@@ -1,6 +1,6 @@
 // whatsapp
 function redirectToWhatsApp() {
-    var phoneNumber = "+919099903098"; // Replace with your WhatsApp number
+    var phoneNumber = "+919265738337"; // Replace with your WhatsApp number
     var message = ""; // Optional: Replace with your default message
     var url =
       "https://api.whatsapp.com/send?phone=" +
